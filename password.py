2a1ca96e5f2201abc299ce31dd9eb43181180776
Joy Kimani <128522373+Joy-Kimani@users.noreply.github.com>
2023-12-13 12:34:55 +0300

    Update password.py

--- password.py
+++ password.py
@@ -1,10 +1,12 @@
 import random
-
+#define the characters
 chars="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%^&(*)"
 
 while 1:
+    #user input
     password_len=int(input("What length would you like your password to be : "))
     password_count=int(input("How many passwords would you like : "))
+    #password generation
     for x in range(0,password_count):
         password=""
         for x in range(0,password_len):
